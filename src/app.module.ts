80c524281b54cb166f6d29a1d37414f86cbaac67
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2018-06-03 00:43:43 +0300

    [maintenance]:fixing typos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
+import { Module } from '@nestjs/common';
 import * as passport from 'passport';
-// import * as passport from 'passport';
-import { buildSchema } from 'graphql';
-import { AuthModule } from './auth/auth.module';
+
+import { GraphqlModule } from 'modules/graphql/graphql.module';
+import { AuthModule } from 'modules/auth/auth.module';
 
 @Module({
   imports: [GraphqlModule, AuthModule],
 })
-
-export class AppModule implements NestModule {
-  constructor(private readonly graphQLFactory: GraphQLFactory) { }
-  configure(consumer: MiddlewareConsumer) {
-    const schema = buildSchema(`
-    const root = {
-      message: () => 'Welcome to My-Special-W@@y!',
-    };
-    consumer
-      .apply(passport.initialize())
-      .forRoutes('/graphql')
-      .apply(passport.authenticate('jwt', { session: false }))
-      .forRoutes('/graphql')
-      .apply(graphqlExpress(req => ({ schema, rootValue: root })))
-      .forRoutes('/graphql')
-      .apply(graphiqlExpress({ endpointURL: '/graphql' }))
-      .forRoutes('/graphiql');
-  }
+export class AppModule {}
