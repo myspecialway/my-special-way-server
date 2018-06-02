52733aa88409d2598de4f0eb949b181b5c0b2588
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2018-06-03 00:49:10 +0300

    [maintenance]:fixing paths for e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import * as passport from 'passport';
 
-import { GraphqlModule } from 'modules/graphql/graphql.module';
-import { AuthModule } from 'modules/auth/auth.module';
+import { GraphqlModule } from './modules/graphql/graphql.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [GraphqlModule, AuthModule],
