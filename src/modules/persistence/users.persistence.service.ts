ef03d0777ebecf6920b42087f48e96c583883acd
Sidis, Itay (is874k) <is874k@intl.att.com>
2018-06-16 00:02:46 +0300

    student resolver getClassStudents

--- src/modules/persistence/users.persistence.service.ts
+++ src/modules/persistence/users.persistence.service.ts
@@ -6,58 +6,77 @@ import { UserLoginRequest } from 'models/user-login-request.model';
 
 @Injectable()
 export class UsersPersistenceService extends Logger {
-    private _collection: Collection<UserDbModel>;
-    constructor(private dbService: DbService) {
-        super('UsersPersistenceService');
-    }
+  private _collection: Collection<UserDbModel>;
+  constructor(private dbService: DbService) {
+    super('UsersPersistenceService');
+  }
 
-    private get collection() {
-        if (this._collection) {
-            return this._collection;
-        }
-        const db = this.dbService.getConnection();
-        this._collection = db.collection<UserDbModel>('users');
-        return this._collection;
+  private get collection() {
+    if (this._collection) {
+      return this._collection;
     }
+    const db = this.dbService.getConnection();
+    this._collection = db.collection<UserDbModel>('users');
+    return this._collection;
+  }
 
-    async getAll() {
-        try {
-            this.log('getAll:: fetching users');
-            return this.collection.find({}).toArray();
-        } catch (error) {
-            this.error('getAll:: error fetching users', error.stack);
-            throw error;
-        }
+  async getAll() {
+    try {
+      this.log('getAll:: fetching users');
+      return this.collection.find({}).toArray();
+    } catch (error) {
+      this.error('getAll:: error fetching users', error.stack);
+      throw error;
     }
+  }
 
-    async getById(id: string) {
-        try {
-            const _id = new ObjectID(id);
-            this.log(`getAll:: fetching user by id ${id}`);
-            return this.collection.findOne({ _id });
-        } catch (error) {
-            this.error(`getAll:: error fetching user by id ${id}`, error.stack);
-            throw error;
-        }
+  async getById(id: string) {
+    try {
+      const _id = new ObjectID(id);
+      this.log(`getAll:: fetching user by id ${id}`);
+      return this.collection.findOne({ _id });
+    } catch (error) {
+      this.error(`getAll:: error fetching user by id ${id}`, error.stack);
+      throw error;
     }
+  }
 
-    async authenticateUser({ username, password }: UserLoginRequest): Promise<[Error, UserDbModel]> {
-        try {
-            this.log(`authenticateUser:: authenticating user ${username}`);
-            return [null, await this.collection.findOne({ username, password })];
-        } catch (error) {
-            this.error(`authenticateUser:: error authenticating user ${username}`, error.stack);
-            return [error, null];
-        }
+  async authenticateUser({
+    username,
+    password,
+  }: UserLoginRequest): Promise<[Error, UserDbModel]> {
+    try {
+      this.log(`authenticateUser:: authenticating user ${username}`);
+      return [null, await this.collection.findOne({ username, password })];
+    } catch (error) {
+      this.error(
+        `authenticateUser:: error authenticating user ${username}`,
+        error.stack,
+      );
+      return [error, null];
     }
+  }
 
-    async getByUsername(username: string): Promise<[Error, UserDbModel]> {
-        try {
-            this.log(`getByUsername:: fetching user by username ${username}`);
-            return [null, await this.collection.findOne({ username })];
-        } catch (error) {
-            this.error(`getAll:: error fetching user by username ${username}`, error.stack);
-            return [error, null];
-        }
+  async getByUsername(username: string): Promise<[Error, UserDbModel]> {
+    try {
+      this.log(`getByUsername:: fetching user by username ${username}`);
+      return [null, await this.collection.findOne({ username })];
+    } catch (error) {
+      this.error(
+        `getAll:: error fetching user by username ${username}`,
+        error.stack,
+      );
+      return [error, null];
     }
+  }
+
+  async getClassStudents(class_id: string) {
+    try {
+        this.log(`getClassStudents: fetching students of class id ${class_id}`);
+        return await this.collection.find({ class_id, role: 'STUDENT' }).toArray();
+    } catch (error) {
+      this.error(`getClassStudents:: error fetching students by class id ${class_id}`, error.stack);
+      throw error;
+    }
+  }
 }
