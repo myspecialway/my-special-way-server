a6281eded7519ed2cd87d44f745450db2e04af73
Danny Vernovsky <dvernovsky@interwise.com>
2018-06-16 16:53:29 +0300

    🚨 fixed lint errors

--- src/config/environments/dev.ts
+++ src/config/environments/dev.ts
@@ -4,7 +4,6 @@ import { Config } from './config.model';
 export const config: Config = {
     db: {
         connectionString: 'mongodb://admin:Aa123456@ds016118.mlab.com:16128/msw-dev',
-        // connectionString: 'mongodb://localhost',
         dbName: 'msw-dev',
     },
 };
