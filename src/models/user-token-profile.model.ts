acb20efdd7584b9c75a4933b1197a301354ed949
Danny Vernovsky <dvernovsky@interwise.com>
2018-07-12 19:16:43 +0300

    ♻️ added missing fields to token payload and tests to auth.service

--- src/models/user-token-profile.model.ts
+++ src/models/user-token-profile.model.ts
@@ -1,6 +1,9 @@
 import { UserRole } from './user.db.model';
 
 export interface UserTokenProfile {
+    id: string;
     username: string;
     role: UserRole;
+    firstname: string;
+    lastname: string;
 }
---
+++ src/modules/auth/auth-service/auth.service.spec.ts
@@ -0,0 +1,31 @@
+import { AuthServiceInterface } from './auth.service.interface';
+import { AuthService } from './auth.service';
+import { UsersPersistenceService } from '../../persistence/users.persistence.service';
+import { UserDbModel } from '../../../models/user.db.model';
+import { UserLoginRequest } from '../../../models/user-login-request.model';
+
+describe('auth.service', () => {
+    let authService: AuthServiceInterface;
+    let userPersistanceServiceMock: Partial<UsersPersistenceService>;
+
+    beforeEach(() => {
+        userPersistanceServiceMock = {
+            authenticateUser: jest.fn(),
+        };
+
+        authService = new AuthService(userPersistanceServiceMock as UsersPersistenceService);
+
+    });
+
+    it('should create valid token if user was found in db', async () => {
+        expect.hasAssertions();
+        // given
+        (userPersistanceServiceMock.authenticateUser as jest.Mock).mockReturnValueOnce([null, {
+            username: 'mock user',
+        } as UserDbModel]);
+
+        // when
+        const token = await authService.createTokenFromCridentials({} as UserLoginRequest);
+        expect(token).toBeDefined();
+    });
+});
