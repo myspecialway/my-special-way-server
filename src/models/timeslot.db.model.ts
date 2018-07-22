89da91e3f96b6d62f66506db5f739631dd97e7bf
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2018-07-22 15:09:46 +0300

    [dev] - locations endpoint  - COMMIT 2

--- src/models/timeslot.db.model.ts
+++ src/models/timeslot.db.model.ts
@@ -1,5 +1,5 @@
 import { LessonDbModel } from './lesson.db.model';
-import RoomDbModel from './room.db.model';
+import RoomDbModel from './location.db.model';
 
 // Describes a single timeslot on the schedule
 export interface TimeSlotDbModel {
--- src/modules/persistence/persistence.module.ts
+++ src/modules/persistence/persistence.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { getDbServiceProvider } from './db.service';
-import { getConfig } from '../../config/config-loader';
+import { getConfig } from 'config/config-loader';
 import { ClassPersistenceService } from './class.persistence.service';
 import { UsersPersistenceService } from './users.persistence.service';
 import { LessonPersistenceService } from './lesson.persistence.service';
 import { SchedulePersistenceService } from './schedule.persistence.service';
+import {LocationsPersistenceService } from './locations.persistence.service';
 
 @Module({
     providers: [
@@ -13,12 +14,14 @@ import { SchedulePersistenceService } from './schedule.persistence.service';
         ClassPersistenceService,
         LessonPersistenceService,
         SchedulePersistenceService,
+        LocationsPersistenceService
     ],
     exports: [
         UsersPersistenceService,
         ClassPersistenceService,
         LessonPersistenceService,
         SchedulePersistenceService,
+        LocationsPersistenceService
     ],
 })
 export class PersistenceModule { }
