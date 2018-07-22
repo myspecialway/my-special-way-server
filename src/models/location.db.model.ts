30064c79ae33818f5f1fc131bb6b0a155d337bce
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2018-07-22 15:07:28 +0300

    [dev] - locations endpoint  - COMMIT 1

---
+++ src/models/location.db.model.ts
@@ -0,0 +1,7 @@
+import { Point } from './point.db.model';
+export default interface LocationDbModel {
+    _id: string;
+    name: string;
+    disabled: boolean;
+    position: Point;
+}
--- src/models/room.db.model.ts
+++
@@ -1,7 +0,0 @@
-import Point from './point.db.model';
-export default interface RoomDbModel {
-    _id: string;
-    name: string;
-    disabled: boolean;
-    position: Point;
-}
