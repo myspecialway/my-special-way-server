48b9d440972e06b46bc8575f6b6f78ef6a91ae19
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2018-07-23 09:35:22 +0300

    [dev] - locations endpoint  - COMMIT 6

--- src/models/timeslot.db.model.ts
+++ src/models/timeslot.db.model.ts
@@ -1,9 +1,9 @@
 import { LessonDbModel } from './lesson.db.model';
-import RoomDbModel from './location.db.model';
+import LocationDbModel from './location.db.model';
 
 // Describes a single timeslot on the schedule
 export interface TimeSlotDbModel {
     index: string;
     lesson?: LessonDbModel;
-    room?: RoomDbModel;
+    room?: LocationDbModel;
 }
