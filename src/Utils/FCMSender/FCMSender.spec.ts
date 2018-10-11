8c370c53f76a6afe8e5293ee200c5c6442f22b77
KfirMor <kfir.mor@gmail.com>
2018-10-11 16:00:43 +0300

    For Itzik

--- src/Utils/FCMSender/FCMSender.spec.ts
+++ src/Utils/FCMSender/FCMSender.spec.ts
@@ -1,28 +1,37 @@
 jest.mock('firebase-admin');
 import { FCMSender } from './FCMSender';
 import * as admin from 'firebase-admin';
+// import * as AdminRoot from 'firebase-admin-mock';
 
 describe('FCMSender', () => {
     let instance: FCMSender;
-    let strResult: string;
     let bResult: boolean;
     let messageMock;
 
     beforeAll(() => {
-        strResult = '';
         bResult = false;
-        // admin.messaging = jest.fn();
-        const adminMessaging = admin.messaging as jest.Mock;
+        instance = new FCMSender();
+    });
+
+    beforeEach(() => {
+        // const adminMessaging = admin.messaging as jest.Mock;
+        const AdminMessaging = jest.fn<admin.messaging.Messaging>(() => ({
+            send: jest.fn(),
+        }));
+
+        const adminMessaging = new AdminMessaging();
+
         messageMock = {
             send: jest.fn(),
         };
 
         messageMock.send.mockReturnValue = true;
-        adminMessaging.mockReturnValue(messageMock);
-    });
+        //  adminMessaging.mockReturnValue(messageMock);
+        // (admin.messaging as jest.Mock).mockImplementation(() => {
 
-    beforeEach(() => {
-        instance = new FCMSender();
+        // adminMessaging.mockImplementation(() => {
+        //     messageMock();
+        // });
     });
 
     // Testing an attempt to send a message before initializing any details
@@ -30,21 +39,24 @@ describe('FCMSender', () => {
         // given
 
         // when
-        bResult = await instance.sendTxtMsgToAndroid('sToken', '', '' );
+//        bResult = await instance.sendTxtMsgToAndroid('sToken', '', '' );
         // then
 
-        // instance = new FCMSender();
-        // expect(instance).toBeInstanceOf(FCMSender);
+        expect(instance).toBeInstanceOf(FCMSender);
         bResult = await instance.sendTxtMsgToAndroid('sToken', 'sBody', 'sSbj');
-        expect(bResult).toBe(false);
+        expect(messageMock.send).toHaveBeenCalled();
+        // expect(messageMock.sendTxtMsgToAndroid).toHaveBeenCalledWith(
+        //     {clientToken: 'aaa', messageBody: 'bbb', messageTitle: 'ccc'},
+        //     expect.any(Function));
+
+        // expect(bResult).toBe(false);
     });
 
     // Testing an attempt to send a message using mock functions
     it('should succeed when sending Android push text message', async () => {
         expect(instance).toBeInstanceOf(FCMSender);
-        bResult = await instance.sendTxtMsgToAndroid('sToken', 'sBody', 'sSbj');
-        //        expect(messageMock.send).toHaveBeenCalled();
-        expect(messageMock.send).toBe( true );
+        // bResult = await instance.sendTxtMsgToAndroid('sToken', 'sBody', 'sSbj');
+        // expect(bResult).toBe( true );
     });
 
 });
