42059886a230ad88235066a4338e140695a91297
Rechtman, Maayan(mr372e) <mr372e@intl.att.com>
2018-10-14 14:20:27 +0300

    #281 - Implemented updateModel

--- src/modules/graphql/resolvers/locations.resolver.ts
+++ src/modules/graphql/resolvers/locations.resolver.ts
@@ -1,5 +1,10 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { LocationsPersistenceService } from '../../persistence/locations.persistence.service';
+import { Asset, checkAndGetBasePermission, DBOperation, NO_PERMISSION, Permission } from '../../permissions/permission.interface';
+import { Get } from '../../../utils/get';
+import { UserDbModel } from '../../../models/user.db.model';
+import { ClassDbModel } from '../../../models/class.db.model';
+import LocationDbModel from '../../../models/location.db.model';
 
 @Resolver('Locations')
 export class LocationsResolver {
@@ -9,4 +14,24 @@ export class LocationsResolver {
     async getLocations(_, {}, context) {
         return this.locationsPersistence.getAll();
     }
+
+  @Query('locationById')
+  async getLocationById(obj, args, context, info) {
+    return await this.locationsPersistence.getById(args.id);
+  }
+
+  @Mutation('createLocation')
+  async createLocation(_, { location }, context) {
+    return this.locationsPersistence.createLocation(location);
+  }
+
+  @Mutation('updateLocation')
+  async updateLocation(_, {id, location}, context) {
+    return this.locationsPersistence.updateLocation(id, location);
+  }
+
+  @Mutation('deleteLocation')
+  async deleteLocation(_, {id}, context) {
+    return this.locationsPersistence.deleteLocation(id);
+  }
 }
--- src/modules/persistence/locations.persistence.service.ts
+++ src/modules/persistence/locations.persistence.service.ts
@@ -1,26 +1,75 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { DbService } from './db.service';
-import { Collection } from 'mongodb';
+import { Collection, ObjectID } from 'mongodb';
 import LocationDbModel from 'models/location.db.model';
 import { ILocationsPersistenceService } from './interfaces/locations.persistence.service.interface';
 
 @Injectable()
 export class LocationsPersistenceService implements ILocationsPersistenceService {
-    private collection: Collection<LocationDbModel>;
-    private logger = new Logger('LocationsPersistenceService');
+  private collection: Collection<LocationDbModel>;
+  private logger = new Logger('LocationsPersistenceService');
 
-    constructor(private dbService: DbService) {
-        const db = this.dbService.getConnection();
-        this.collection = db.collection<LocationDbModel>('locations');
-    }
+  constructor(private dbService: DbService) {
+    const db = this.dbService.getConnection();
+    this.collection = db.collection<LocationDbModel>('locations');
+  }
 
-    async getAll(): Promise<LocationDbModel[]> {
-        try {
-            this.logger.log('getAll:: fetching locations');
-            return await this.collection.find({}).sort({ name: 1 }).toArray();
-        } catch (error) {
-            this.logger.error('getAll:: error fetching locations', error.stack);
-            throw error;
-        }
+  async getAll(): Promise<LocationDbModel[]> {
+    try {
+      this.logger.log('getAll:: fetching locations');
+      return await this.collection.find({}).sort({ name: 1 }).toArray();
+    } catch (error) {
+      this.logger.error('getAll:: error fetching locations', error.stack);
+      throw error;
     }
+  }
+
+  async getById(id: string) {
+    try {
+      const mongoId = new ObjectID(id);
+      this.logger.log(`getAll:: fetching location by id ${id}`);
+      return await this.collection.findOne({ _id: mongoId });
+    } catch (error) {
+      this.logger.error(`getAll:: error location class by id ${id}`, error.stack);
+      throw error;
+    }
+  }
+
+  async createLocation(location: LocationDbModel): Promise<LocationDbModel> {
+    try {
+      this.logger.log(`LocationsPersistenceService::createLocation:: create location`);
+      const insertResponse = await this.collection.insertOne(location);
+      return await this.getById(insertResponse.insertedId.toString());
+    } catch (error) {
+      this.logger.error('ClassPersistenceService::createClass:: error creating class', error.stack);
+      throw error;
+    }
+  }
+
+  async updateLocation(id: string, locationObj: LocationDbModel): Promise<LocationDbModel> {
+    const mongoId = new ObjectID(id);
+    try {
+      this.logger.log(`LocationPersistence::updateLocation:: updating location ${mongoId}`);
+      const updatedDocument = await this.collection.findOneAndUpdate({ _id: mongoId },
+        { $set: { ...locationObj } }, { returnOriginal: false });
+      this.logger.log(`LocationPersistence::updateLocation:: updated DB :${JSON.stringify(updatedDocument.value)}`);
+      return updatedDocument.value;
+    } catch (error) {
+      this.logger.error(`LocationPersistence::updateLocation:: error updating location ${mongoId}`, error.stack);
+      throw error;
+    }
+  }
+
+  async deleteLocation(id: string): Promise<number> {
+    try {
+      const mongoId = new ObjectID(id);
+      this.logger.log(`LocationPersistence::deleteLocation:: deleting location by id ${id}`);
+      const deleteResponse = await this.collection.deleteOne({ _id: mongoId });
+      this.logger.log(`LocationPersistence::deleteLocation:: removed ${deleteResponse.deletedCount} documents`);
+      return deleteResponse.deletedCount;
+    } catch (error) {
+      this.logger.error(`LocationPersistence::deleteLocation:: error deleting class by id ${id}`, error.stack);
+      throw error;
+    }
+  }
 }
