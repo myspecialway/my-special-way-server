25ab9a27552e5d065dc2777e8253ffbcbcbf4484
rmaayan1 <maayan.rechtman@gmail.com>
2018-10-15 15:20:49 +0300

    281 - minimized number of parameters

--- src/modules/graphql/resolvers/lesson.resolver.ts
+++ src/modules/graphql/resolvers/lesson.resolver.ts
@@ -8,25 +8,25 @@ export class LessonResolver {
   constructor(private lessonPersistence: LessonPersistenceService) {}
 
   @Query('lessons')
-  async getLessons(_, {}, context) {
+  async getLessons(context) {
       checkAndGetBasePermission(Get.getObject(context, 'user'), DBOperation.READ, Asset.LESSON);
       return this.lessonPersistence.getAll();
   }
 
   @Mutation('createLesson')
-  async createLesson(_, { lesson }, context) {
+  async createLesson({ lesson }, context) {
       checkAndGetBasePermission(Get.getObject(context, 'user'), DBOperation.CREATE, Asset.LESSON);
       return this.lessonPersistence.createLesson(lesson);
   }
 
   @Mutation('updateLesson')
-  async updateLesson(_, {id, lesson}, context) {
+  async updateLesson({id, lesson}, context) {
       checkAndGetBasePermission(Get.getObject(context, 'user'), DBOperation.UPDATE, Asset.LESSON);
       return this.lessonPersistence.updateLesson(id, lesson);
   }
 
   @Mutation('deleteLesson')
-  async deleteLesson(_, {id}, context) {
+  async deleteLesson({id}, context) {
       checkAndGetBasePermission(Get.getObject(context, 'user'), DBOperation.DELETE, Asset.LESSON);
       return this.lessonPersistence.deleteLesson(id);
   }
