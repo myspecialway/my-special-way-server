0a0f3408373002bb04dc80415160e511417c125d
dkhnn <david.khanin@gmail.com>
2018-10-16 18:05:19 +0300

    added papertrail

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PaperTrailLogger } from './utils/papertrail.logger';
 
 const instance = express();
 /* Express middleware.  */
@@ -11,7 +12,9 @@ instance.use(bodyParser.urlencoded({ extended: false }));
 /* End of express middleware. */
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, instance);
+  const app = await NestFactory.create(AppModule, instance, {
+    logger: new PaperTrailLogger(),
+  });
   app.enableCors();
   app.useStaticAssets(path.join(__dirname, './public'));
   await app.listen(3000);
---
+++ src/utils/papertrail.logger.ts
@@ -0,0 +1,39 @@
+import { LoggerService, Logger } from '@nestjs/common';
+import * as winston from 'winston';
+import { Papertrail } from 'winston-papertrail';
+import { getConfig } from '../config/config-loader';
+
+export class PaperTrailLogger extends Logger implements LoggerService {
+
+    constructor(context?: string, isTimeDiffEnabled?: boolean) {
+        super(context, isTimeDiffEnabled);
+        // if (getConfig().isProd) {
+        //     this.transportList.push(this.winstonPapertrail);
+         }
+
+        winstonPapertrail = new Papertrail({
+            host: 'logs7.papertrailapp.com',
+            port: 32979,
+        });
+        logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.prettyPrint(),
+            transports: [
+                new winston.transports.File({ filename: 'combined.log' }),
+                new winston.transports.Console(),
+            ],
+        });
+        log(message: string) {
+            this.logger.info(message);
+            super.log(message);
+        }
+        error(message: string, trace: string) {
+            this.logger.error(message);
+            super.error(message, trace);
+        }
+        warn(message: string) {
+            this.logger.warn(message);
+            super.warn(message);
+        }
+
+    }
