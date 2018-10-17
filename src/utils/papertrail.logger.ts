30fa9dd6671a6d192e410b3934b1e3d1910860de
dkhnn <david.khanin@gmail.com>
2018-10-17 10:58:15 +0300

    only in main

--- src/utils/papertrail.logger.ts
+++ src/utils/papertrail.logger.ts
@@ -1,39 +1,34 @@
-import { LoggerService, Logger } from '@nestjs/common';
+import { LoggerService } from '@nestjs/common';
 import * as winston from 'winston';
-// import { Papertrail } from 'winston-papertrail';
+import { Papertrail } from 'winston-papertrail';
 // import { getConfig } from '../config/config-loader';
 
-export class PaperTrailLogger extends Logger implements LoggerService {
+export class PaperTrailLogger implements LoggerService {
 
-    constructor(context?: string, isTimeDiffEnabled?: boolean) {
-        super(context, isTimeDiffEnabled);
+    constructor() {
         // if (getConfig().isProd) {
         //     this.transportList.push(this.winstonPapertrail);
          }
 
-        // winstonPapertrail = new Papertrail({
-        //     host: 'logs7.papertrailapp.com',
-        //     port: 32979,
-        // });
+        winstonPapertrail = new Papertrail({
+            host: 'logs7.papertrailapp.com',
+            port: 32979,
+        });
         logger = winston.createLogger({
             level: 'info',
-            format: winston.format.prettyPrint(),
+            format: winston.format.simple(),
             transports: [
-                new winston.transports.File({ filename: 'combined.log' }),
                 new winston.transports.Console(),
             ],
         });
         log(message: string) {
             this.logger.info(message);
-            super.log(message);
         }
         error(message: string, trace: string) {
             this.logger.error(message);
-            super.error(message, trace);
         }
         warn(message: string) {
             this.logger.warn(message);
-            super.warn(message);
         }
 
     }
