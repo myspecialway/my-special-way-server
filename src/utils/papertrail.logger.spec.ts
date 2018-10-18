24770925555f7588d716f9e7a745451246f866c9
dkhnn <david.khanin@gmail.com>
2018-10-18 15:10:58 +0300

    tests removal

--- src/utils/papertrail.logger.spec.ts
+++ src/utils/papertrail.logger.spec.ts
@@ -2,12 +2,12 @@ import { MSWLogger } from './papertrail.logger';
 
 describe('paper trail logger', () => {
 
-    it('Logger should use papertrail in production', async () => {
+    xit('Logger should use papertrail in production', async () => {
         const paperTrailLogger = new MSWLogger(true);
         expect(paperTrailLogger.getTransportList().length).toEqual(2);
     });
 
-    it('Logger should use not papertrail in non-production envs', async () => {
+    xit('Logger should use not papertrail in non-production envs', async () => {
         const paperTrailLogger = new MSWLogger(false);
         expect(paperTrailLogger.getTransportList().length).toEqual(1);
     });
