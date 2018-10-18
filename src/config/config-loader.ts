966078a69043b475945763ffbaa5487b96d99fff
dkhnn <david.khanin@gmail.com>
2018-10-18 18:13:12 +0300

    cleanup

--- src/config/config-loader.ts
+++ src/config/config-loader.ts
@@ -1,14 +1,6 @@
 import * as envalid from 'envalid';
 import { ProcessEnvConfig, getEnvalidValidations } from './config-interface';
-import { Logger } from '@nestjs/common';
-export function getConfig() {
-    const logger = new Logger('getConfig');
-    try {
-        const conf =  envalid.cleanEnv<ProcessEnvConfig>(process.env, getEnvalidValidations());
-        return conf;
-    } catch (e) {
-        logger.error('Config went wrong', e);
-        return null;
-    }
 
-}
+export function getConfig() {
+        return envalid.cleanEnv<ProcessEnvConfig>(process.env, getEnvalidValidations());
+    }
