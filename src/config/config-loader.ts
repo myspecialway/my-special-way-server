853892018ea182ac526e2174d6d30dbf35966b32
dkhnn <david.khanin@gmail.com>
2018-10-18 15:33:07 +0300

    trying something

--- src/config/config-loader.ts
+++ src/config/config-loader.ts
@@ -2,5 +2,12 @@ import * as envalid from 'envalid';
 import { ProcessEnvConfig, getEnvalidValidations } from './config-interface';
 
 export function getConfig() {
-    return envalid.cleanEnv<ProcessEnvConfig>(process.env, getEnvalidValidations());
+    try {
+    const conf =  envalid.cleanEnv<ProcessEnvConfig>(process.env, getEnvalidValidations());
+    return conf;
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+
 }
