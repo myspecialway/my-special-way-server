ca9293d6406f6c9fa397e9a839ca5681bb678034
KfirMor <kfir.mor@gmail.com>
2018-10-20 23:01:00 +0300

    Updated the FCM Data payload

--- src/Utils/FCMSender/FCM.data.ts
+++ src/Utils/FCMSender/FCM.data.ts
@@ -1,10 +1,16 @@
+/* istanbul ignore next */
 export interface FCMData {
     data?: {[key: string]: string};
   }
 
+/* istanbul ignore next */
 export const DATA: FCMData = {
     data : {
-        title : 'hello world',
-        time: '10:45',
+        TYPE : 'Update',
+        PERSONAL_DETAILS : '10:45',
+        SCHEDULE : '10:45',
+        REMINDERS : '10:45',
+        ROUTES : '10:45',
+        GENERAL_SETTINGS : '10:45',
     },
 };
