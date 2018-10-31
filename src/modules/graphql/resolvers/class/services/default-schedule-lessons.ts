908ab8d8b79f0d31093f7837295894e99fab32eb
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-10-31 18:17:24 +0200

    align helpers with new interfaces

---
+++ src/modules/graphql/resolvers/class/services/default-schedule-lessons.ts
@@ -0,0 +1,24 @@
+export const studentAcceptanceLesson = {
+  title: 'קבלת תלמידים',
+  icon: 'no-icon',
+};
+
+export const breakfastLesson = {
+  title: 'ארוחת בוקר',
+  icon: 'breakfest-icon',
+};
+
+export const recessLesson = {
+  title: 'הפסקה',
+  icon: 'break-icon',
+};
+
+export const lunchLesson = {
+  title: 'ארוחת צהריים',
+  icon: 'lunch-icon',
+};
+
+export const dismissLesson = {
+  title: 'פיזור תלמידים',
+  icon: 'dismiss-icon',
+};
