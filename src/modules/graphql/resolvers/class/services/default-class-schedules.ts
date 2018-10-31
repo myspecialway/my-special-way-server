fbe78e29f10dea3f7f496c0f41c5480729263942
Danny Vernovsky <dvernovsky@interwise.com>
2018-10-31 20:56:27 +0200

    🚑 added missing hours to default class schedule resource

--- src/modules/graphql/resolvers/class/services/default-class-schedules.ts
+++ src/modules/graphql/resolvers/class/services/default-class-schedules.ts
@@ -1,6 +1,8 @@
+import { TimeSlotDbModel } from "models/timeslot.db.model";
+
 const studentAcceptanceLesson = {
-    title: 'קבלת תלמידים',
-    icon: 'no-icon',
+  title: 'קבלת תלמידים',
+  icon: 'no-icon',
 };
 
 const breakfastLesson = {
@@ -23,296 +25,368 @@ const dismissLesson = {
   icon: 'dismiss-icon',
 };
 
-export const ELMENTARY_SCHEDULE = [
+export const ELMENTARY_SCHEDULE: TimeSlotDbModel[] = [
   {
     index: '0_0',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_1',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_2',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_3',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_4',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_5',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '3_0',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '3_1',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '3_2',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '3_3',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '3_4',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '3_5',
+    hours: '09:35 - 10:00',
     lesson: breakfastLesson,
   },
   {
     index: '4_0',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '4_1',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '4_2',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '4_3',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '4_4',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '4_5',
+    hours: '10:00 - 10:30',
     lesson: recessLesson,
   },
   {
     index: '8_0',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '8_1',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '8_2',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '8_3',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '8_4',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '8_5',
+    hours: '12:45 - 13:15',
     lesson: lunchLesson,
   },
   {
     index: '9_0',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '9_1',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '9_2',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '9_3',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '9_4',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '9_5',
+    hours: '13:15 - 13:45',
     lesson: recessLesson,
   },
   {
     index: '14_0',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_1',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_2',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_3',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_4',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_5',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
 ];
 
-export const JUNIOR_HIGH = [
+export const JUNIOR_HIGH: TimeSlotDbModel[] = [
   {
     index: '0_0',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_1',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_2',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_3',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_4',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '0_5',
+    hours: '07:30 - 08:00',
     lesson: studentAcceptanceLesson,
   },
   {
     index: '3_0',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '3_1',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '3_2',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '3_3',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '3_4',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '3_5',
+    hours: '09:35 - 10:00',
     lesson: recessLesson,
   },
   {
     index: '4_0',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '4_1',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '4_2',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '4_3',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '4_4',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '4_5',
+    hours: '10:00 - 10:30',
     lesson: breakfastLesson,
   },
   {
     index: '8_0',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '8_1',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '8_2',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '8_3',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '8_4',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '8_5',
+    hours: '12:45 - 13:15',
     lesson: recessLesson,
   },
   {
     index: '9_0',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '9_1',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '9_2',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '9_3',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '9_4',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '9_5',
+    hours: '13:15 - 13:45',
     lesson: lunchLesson,
   },
   {
     index: '14_0',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_1',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_2',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_3',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_4',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
   {
     index: '14_5',
+    hours: '16:30 - 16:45',
     lesson: dismissLesson,
   },
 ];
