645eb9bc3ad3996947a3d15381bd9b3eb2e8e45d
Ulman, Avi (au2811) <au2811@intl.att.com>
2018-11-13 16:54:41 +0200

    #332_add_username_uniqueness_validation

--- src/modules/auth/auth-service/auth.service.interface.ts
+++ src/modules/auth/auth-service/auth.service.interface.ts
@@ -1,7 +1,9 @@
 import { UserLoginRequest } from 'models/user-login-request.model';
 import { UserDbModel } from '../../../models/user.db.model';
+import { UserUniqueValidationRequest } from 'models/user-unique-validation-request.model';
 
 export interface AuthServiceInterface {
-    createTokenFromCridentials(userLogin: UserLoginRequest): Promise<[Error, string]>;
-    validateUserByCridentials(userLogin: UserLoginRequest): Promise<[Error, UserDbModel]>;
+  createTokenFromCridentials(userLogin: UserLoginRequest): Promise<[Error, string]>;
+  validateUserByCridentials(userLogin: UserLoginRequest): Promise<[Error, UserDbModel]>;
+  validateUserNameUnique(userUniqueValidation: UserUniqueValidationRequest): Promise<[Error, boolean]>;
 }
