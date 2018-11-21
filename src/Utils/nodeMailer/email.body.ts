3e7cf8d6c9a82f61399204d4586297401367a5d1
Shitrit, Tal David (ts639c) <ts639c@intl.att.com>
2018-11-21 13:32:25 +0200

    [feature] mail template without link

--- src/Utils/nodeMailer/email.body.ts
+++ src/Utils/nodeMailer/email.body.ts
@@ -8,14 +8,3 @@ export const msg: EmailBody = {
   text: 'Hello world (text version)',
   html: '<P>Hello ✔ (HTML version)<P>',
 };
-
-export const restoreTemplate: string = `
-<div class="email-content" style="text-align: right">
-  <h1>שחזור ססמא למערכת בדרכי שלי </h1>
-  <p>שלום שם פרטי שם משפחה>,</p>
-  <p>אנו שולחים לך לינק חדש לכניסה למערכת.</p>
-  <p>על מנת להתחיל להשתמש במערכת, יש ללחוץ על הלינק הבא ולהגדיר את סיסמתך:</p>
-  <a href="#">http://www.example.com</a>
-  <p>תודה!</p>
-</div>
-`;
--- src/modules/auth/auth-controller/auth.controller.ts
+++ src/modules/auth/auth-controller/auth.controller.ts
@@ -4,7 +4,6 @@ import { UserLoginRequest } from '../../../models/user-login-request.model';
 import { AuthService } from '../auth-service/auth.service';
 import { UserUniqueValidationRequest } from '../../../models/user-unique-validation-request.model';
 import { sendemail } from '../../../utils/nodeMailer/email.client';
-import { restoreTemplate } from '../../../utils/nodeMailer/email.body';
 
 @Controller()
 export class AuthController {
@@ -50,38 +49,33 @@ export class AuthController {
   @Post('/restore-password')
   async restorePassword(@Res() res: Response, @Body() body: any): Promise<void> {
     if (!body) {
-      return this.noBodyError('login', res);
+      return this.noBodyError('restorePassword', res);
     }
-    const result = await sendemail('taltal115@gmail.com', 'taltal115@gmail.com', 'subject', restoreTemplate);
-    this.logger.log(`restorePassword:: restorePassword request for ${result}`);
-
-    // this.logger.log(`login:: login request for ${body.username}`);
-    // const [error, token] = await this.authService.createTokenFromCridentials(body);
-    //
-    // if (error) {
-    //   this.logger.error(`login:: error while logging in ${body.username}`, error.stack);
-    //   res.status(500).json({
-    //     error: 'server error',
-    //     message: 'unknown server error',
-    //   });
-    //
-    //   return;
-    // }
-    //
-    // if (!token) {
-    //   this.logger.warn(`login:: token wasnt created for ${body.username}`);
-    //   res.status(401).json({
-    //     error: 'unauthenticated',
-    //     message: 'username of password are incorrect',
-    //   });
-    //
-    //   return;
-    // }
-    //
-    // this.logger.log(`login:: token ${token} created for ${body.username}`);
-    res.json({
-      status: 'ok',
-    });
+    this.logger.log(JSON.stringify(body.email));
+    try {
+      const restoreTemplate: string = `
+        <div class="email-content" style="text-align: right">
+          <h1>שחזור ססמא למערכת בדרכי שלי</h1>
+          <p>${body.firstname} ${body.lastname} שלום</p>
+          <p>.אנו שולחים לך לינק חדש לכניסה למערכת</p>
+          <p>:על מנת להתחיל להשתמש במערכת, יש ללחוץ על הלינק הבא ולהגדיר את סיסמתך</p>
+          <a href="#">http://www.example.com</a>
+          <p>!תודה</p>
+        </div>
+        `;
+      const result = await sendemail('mswemailclient@gmail.com', body.email, 'שחזור ססמא', restoreTemplate);
+      res.status(200).json({
+        status: 'ok',
+      });
+      this.logger.log(`restorePassword:: restorePassword request for ${result}`);
+    } catch (e) {
+      this.logger.error(`login:: error while logging in ${body.email}`, 'e.stack');
+      res.status(422).json({
+        error: 'server error',
+        message: 'unknown server error',
+      });
+      return;
+    }
   }
 
   @Post('/validateUserNameUnique')
