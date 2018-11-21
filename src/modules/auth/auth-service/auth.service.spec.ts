f800a5a303d51c47f62ad7ebfe7e4c9474ffb412
Ulman, Avi (au2811) <au2811@intl.att.com>
2018-11-21 08:54:02 +0200

    #102 create unexpired token for mobile client

--- src/modules/auth/auth-service/auth.service.spec.ts
+++ src/modules/auth/auth-service/auth.service.spec.ts
@@ -1,31 +1,74 @@
+import * as jwt from 'jsonwebtoken';
+
 import { AuthServiceInterface } from './auth.service.interface';
-import { AuthService } from './auth.service';
+import { AuthService, JWT_EXPIRATION } from './auth.service';
 import { UsersPersistenceService } from '../../persistence/users.persistence.service';
-import { UserDbModel } from '../../../models/user.db.model';
+import { UserDbModel, UserRole } from '../../../models/user.db.model';
 import { UserLoginRequest } from '../../../models/user-login-request.model';
 
 describe('auth.service', () => {
-    let authService: AuthServiceInterface;
-    let userPersistanceServiceMock: Partial<UsersPersistenceService>;
+  let authService: AuthServiceInterface;
+  let userPersistanceServiceMock: Partial<UsersPersistenceService>;
 
-    beforeEach(() => {
-        userPersistanceServiceMock = {
-            authenticateUser: jest.fn(),
-        };
+  beforeEach(() => {
+    userPersistanceServiceMock = {
+      authenticateUser: jest.fn(),
+    };
 
-        authService = new AuthService(userPersistanceServiceMock as UsersPersistenceService);
+    authService = new AuthService(userPersistanceServiceMock as UsersPersistenceService);
+  });
 
+  it('should create valid token if user was found in db', async () => {
+    expect.hasAssertions();
+    // given
+    (userPersistanceServiceMock.authenticateUser as jest.Mock).mockReturnValueOnce([
+      null,
+      {
+        username: 'mock user',
+      } as UserDbModel,
+    ]);
+
+    // when
+    const token = (await authService.createTokenFromCridentials({} as UserLoginRequest))[1];
+    expect(token).toBeDefined();
+  });
+
+  it('should create unexpired token if user role is STUDENT', async () => {
+    expect.hasAssertions();
+    // given
+    (userPersistanceServiceMock.authenticateUser as jest.Mock).mockReturnValueOnce([
+      null,
+      {
+        username: 'mock user',
+        role: UserRole.STUDENT,
+      } as UserDbModel,
+    ]);
+
+    // when
+    const token = (await authService.createTokenFromCridentials({} as UserLoginRequest))[1];
+    const decodedToken: any = jwt.decode(token, { json: true });
+    expect(decodedToken.exp).not.toBeDefined();
+  });
+  for (const role in UserRole) {
+    if (role === UserRole.STUDENT) {
+      continue;
+    }
+    it(`should create expired token if user role is ${role}`, async () => {
+      expect.hasAssertions();
+
+      // given
+      (userPersistanceServiceMock.authenticateUser as jest.Mock).mockReturnValueOnce([
+        null,
+        {
+          username: 'mock user',
+          role,
+        } as UserDbModel,
+      ]);
+
+      // when
+      const token = (await authService.createTokenFromCridentials({} as UserLoginRequest))[1];
+      const decodedToken: any = jwt.decode(token, { json: true });
+      expect(decodedToken.exp).toBeDefined();
     });
-
-    it('should create valid token if user was found in db', async () => {
-        expect.hasAssertions();
-        // given
-        (userPersistanceServiceMock.authenticateUser as jest.Mock).mockReturnValueOnce([null, {
-            username: 'mock user',
-        } as UserDbModel]);
-
-        // when
-        const token = await authService.createTokenFromCridentials({} as UserLoginRequest);
-        expect(token).toBeDefined();
-    });
+  }
 });
