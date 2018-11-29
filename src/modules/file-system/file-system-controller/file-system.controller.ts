e9bb0c9ed4a3fc82fca2232edc0d7e2135f8e0b6
Liberman, Pablo (pl1890) <pl1890@att.com>
2018-11-30 01:20:07 +0200

    Crud implemented for map

--- src/modules/file-system/file-system-controller/file-system.controller.ts
+++ src/modules/file-system/file-system-controller/file-system.controller.ts
@@ -1,35 +1,127 @@
-import { Response, Request } from 'express';
+import { Response } from 'express';
 import {
   Controller,
-  Get,
   Res,
   Post,
   Req,
+  Get,
   Body,
   HttpStatus,
   UseInterceptors,
   FileInterceptor,
   UploadedFile,
+  Param,
+  Put,
+  Logger,
+  Delete,
 } from '@nestjs/common';
 import { FileSystemPersistenceService } from '../../persistence/file-system.persistence.service';
 import { FileSystemDbModel } from '@models/file-system.db.model';
-
-@Controller()
+import { AuthService } from '../../../modules/auth/auth-service/auth.service';
+import { UserTokenProfile } from '@models/user-token-profile.model';
+import { UserRole } from '../../../models/user.db.model';
+const NO_PERMISSION = 'not permissions to execute command';
+const ALREADY_EXIST = 'Map already exist, try update (PUT)';
+@Controller('/map')
 export class FileSystemController {
+  private logger = new Logger('FileSystemController');
   constructor(private filePersistenceService: FileSystemPersistenceService) {}
-  @Post('/map')
+  @Post()
   @UseInterceptors(FileInterceptor('mapFilename'))
-  async upload(@UploadedFile() file, @Req() req, @Body() body) {
+  async upload(@UploadedFile() file, @Req() req, @Body() body, @Res() res: Response) {
+    const userProfile: UserTokenProfile = AuthService.getUserProfileFromToken(req.headers.authorization);
+    if (userProfile.role !== UserRole.PRINCIPLE) {
+      res.sendStatus(HttpStatus.FORBIDDEN).send(NO_PERMISSION);
+      return;
+    }
+    const existMap = await this.filePersistenceService.getFileByFilters({ description: body.floor });
+    if (existMap !== null) {
+      res.status(HttpStatus.BAD_REQUEST).send(ALREADY_EXIST);
+      return;
+    }
+    let newFile: FileSystemDbModel = null;
+    try {
+      newFile = {
+        content: file,
+        description: body.floor,
+        filename: file.originalname,
+      };
+      delete newFile.content.fieldname;
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
+    try {
+      await this.filePersistenceService.createFile(newFile);
+      res.status(HttpStatus.CREATED).send();
+    } catch (error) {
+      this.logger.error('this.filePersistenceService.createFile failed');
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+    }
+  }
+
+  @Delete(':floor')
+  @UseInterceptors(FileInterceptor('mapFilename'))
+  async delete(@Param('floor') floor: string, @Req() req, @Body() body, @Res() res: Response) {
+    const userProfile: UserTokenProfile = AuthService.getUserProfileFromToken(req.headers.authorization);
+    if (userProfile.role !== UserRole.PRINCIPLE) {
+      res.sendStatus(HttpStatus.FORBIDDEN).send(NO_PERMISSION);
+      return;
+    }
+    const existMap = await this.filePersistenceService.getFileByFilters({ description: floor });
+    if (!existMap) {
+      res.status(HttpStatus.NOT_FOUND).send();
+      return;
+    }
+    try {
+      await this.filePersistenceService.deleteFile(existMap._id);
+      res.status(HttpStatus.NO_CONTENT).send();
+    } catch (err) {
+      this.logger.error('this.filePersistenceService.updateFile failed');
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+    }
+  }
+
+  @Put(':floor')
+  @UseInterceptors(FileInterceptor('mapFilename'))
+  async update(@Param('floor') floor: string, @UploadedFile() file, @Req() req, @Body() body, @Res() res: Response) {
+    const userProfile: UserTokenProfile = AuthService.getUserProfileFromToken(req.headers.authorization);
+    if (userProfile.role !== UserRole.PRINCIPLE) {
+      res.sendStatus(HttpStatus.FORBIDDEN).send(NO_PERMISSION);
+      return;
+    }
+    const existMap = await this.filePersistenceService.getFileByFilters({ description: floor });
+    if (!existMap) {
+      res.status(HttpStatus.NOT_FOUND).send();
+      return;
+    }
     const newFile: FileSystemDbModel = {
       content: file,
-      description: body.floor,
+      description: floor,
       filename: file.originalname,
     };
     delete newFile.content.fieldname;
-    await this.filePersistenceService.createFile(newFile);
+
+    try {
+      await this.filePersistenceService.updateFile(existMap._id, newFile);
+      res.status(HttpStatus.OK).send();
+    } catch (err) {
+      this.logger.error('this.filePersistenceService.updateFile failed');
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+    }
   }
-  @Get('/map/:floor')
-  async readiness(@Res() res: Response): Promise<void> {
-    res.status(HttpStatus.OK).json({ data: 'success' });
+
+  @Get(':floor')
+  async readiness(@Param('floor') floor: string, @Res() res: Response, @Req() req): Promise<void> {
+    const userProfile: UserTokenProfile = AuthService.getUserProfileFromToken(req.headers.authorization);
+    if (userProfile === null) {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+      return;
+    }
+    const existMap = await this.filePersistenceService.getFileByFilters({ description: floor });
+    if (!existMap) {
+      res.status(HttpStatus.NOT_FOUND).send();
+      return;
+    }
+    res.send(existMap).status(HttpStatus.OK);
   }
 }
--- src/modules/persistence/file-system.persistence.service.ts
+++ src/modules/persistence/file-system.persistence.service.ts
@@ -12,14 +12,47 @@ export class FileSystemPersistenceService {
     const db = this.dbService.getConnection();
     this.collection = db.collection<FileSystemDbModel>('fileSystem');
   }
-  async createFile(newFile: FileSystemDbModel): Promise<boolean> {
+  private buildMongoFilterFromQuery(query: { [id: string]: any }, id?: string): { [id: string]: string } {
+    if (id) {
+      const mongoId = new ObjectID(id);
+      query._id = mongoId;
+    }
+    return query;
+  }
+  async createFile(newFile: FileSystemDbModel): Promise<void> {
     try {
       this.logger.log(`FileSystemPersistenceService createFile`);
-      const insertResponse = await this.collection.insertOne(newFile);
-      const mongoId = new ObjectID(insertResponse.insertedId.toString());
-      return true;
+      await this.collection.insertOne(newFile);
     } catch (error) {
       this.logger.error('FileSystemPersistenceService::createFile:: error create File', error.stack);
+      throw error;
+    }
+  }
+  async getFileByFilters(queyParams: { [id: string]: string }, id?: string): Promise<FileSystemDbModel> {
+    try {
+      const mongoQuery = this.buildMongoFilterFromQuery(queyParams, id);
+      this.logger.log(`getFileByFilters:: fetching file by parameters `);
+      return await this.collection.findOne(mongoQuery);
+    } catch (error) {
+      this.logger.error(`getFileByFilters:: error fetching user by parameters`, error.stack);
+      throw error;
+    }
+  }
+  async updateFile(id: string, newFile: FileSystemDbModel): Promise<void> {
+    try {
+      this.logger.log(`FileSystemPersistenceService updateFile`);
+      await this.collection.findOneAndReplace({ _id: id }, newFile);
+    } catch (error) {
+      this.logger.error('FileSystemPersistenceService::updateFile:: error update File', error.stack);
+      throw error;
+    }
+  }
+  async deleteFile(id: string): Promise<void> {
+    try {
+      this.logger.log(`FileSystemPersistenceService deleteFile`);
+      await this.collection.deleteOne({ _id: id });
+    } catch (error) {
+      this.logger.error('FileSystemPersistenceService::deleteFile:: error delete File', error.stack);
       throw error;
     }
   }
