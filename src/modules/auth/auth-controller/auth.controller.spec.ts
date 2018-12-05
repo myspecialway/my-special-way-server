2fbcc91c65a400ec79c92e725127f1d856eca314
Shitrit, Tal David (ts639c) <ts639c@intl.att.com>
2018-12-05 09:55:47 +0200

    [fix] unit test #1

--- src/modules/auth/auth-controller/auth.controller.spec.ts
+++ src/modules/auth/auth-controller/auth.controller.spec.ts
@@ -22,55 +22,55 @@ describe('auth controller', () => {
     authController = new AuthController(authServiceMock as AuthService);
   });
 
-  // it('should return 400 if no body was passed', async () => {
-  //   const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
-  //   createTokenFn.mockReturnValueOnce([null, 'some-very-secret-token']);
-  //
-  //   await authController.login(responseMock, null);
-  //
-  //   expect(responseMock.status).toHaveBeenCalledWith(400);
-  // });
-  //
-  // it('should return 500 server error if error happened', async () => {
-  //   const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
-  //   createTokenFn.mockReturnValueOnce([new Error('mock error'), null]);
-  //
-  //   await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
-  //
-  //   expect(responseMock.status).toHaveBeenCalledWith(500);
-  //   expect(responseMock.json).toHaveBeenCalledWith({
-  //     error: 'server error',
-  //     message: 'unknown server error',
-  //   });
-  // });
-  //
-  // it('should return 401 if user token returned null', async () => {
-  //   expect.hasAssertions();
-  //
-  //   const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
-  //   createTokenFn.mockReturnValueOnce([null, null]);
-  //
-  //   await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
-  //
-  //   expect(responseMock.status).toHaveBeenCalledWith(401);
-  //   expect(responseMock.json).toHaveBeenCalledWith({
-  //     error: 'unauthenticated',
-  //     message: 'username of password are incorrect',
-  //   });
-  // });
-  //
-  // it('should return response with token if user has been found and password match', async () => {
-  //   expect.hasAssertions();
-  //
-  //   const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
-  //   createTokenFn.mockReturnValueOnce([null, 'some-very-secret-token']);
-  //
-  //   await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
-  //
-  //   expect(responseMock.json).toHaveBeenCalledWith({
-  //     accessToken: 'some-very-secret-token',
-  //   });
-  // });
+  it('should return 400 if no body was passed', async () => {
+    const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
+    createTokenFn.mockReturnValueOnce([null, 'some-very-secret-token']);
+
+    await authController.login(responseMock, null);
+
+    expect(responseMock.status).toHaveBeenCalledWith(400);
+  });
+
+  it('should return 500 server error if error happened', async () => {
+    const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
+    createTokenFn.mockReturnValueOnce([new Error('mock error'), null]);
+
+    await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
+
+    expect(responseMock.status).toHaveBeenCalledWith(500);
+    expect(responseMock.json).toHaveBeenCalledWith({
+      error: 'server error',
+      message: 'unknown server error',
+    });
+  });
+
+  it('should return 401 if user token returned null', async () => {
+    expect.hasAssertions();
+
+    const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
+    createTokenFn.mockReturnValueOnce([null, null]);
+
+    await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
+
+    expect(responseMock.status).toHaveBeenCalledWith(401);
+    expect(responseMock.json).toHaveBeenCalledWith({
+      error: 'unauthenticated',
+      message: 'username of password are incorrect',
+    });
+  });
+
+  it('should return response with token if user has been found and password match', async () => {
+    expect.hasAssertions();
+
+    const createTokenFn = authServiceMock.createTokenFromCridentials as jest.Mock<Promise<[Error, string]>>;
+    createTokenFn.mockReturnValueOnce([null, 'some-very-secret-token']);
+
+    await authController.login(responseMock, { username: 'mock-user', password: 'mock-password' });
+
+    expect(responseMock.json).toHaveBeenCalledWith({
+      accessToken: 'some-very-secret-token',
+    });
+  });
   it('should return response with status ok if mail as been sent', async () => {
     expect.hasAssertions();
 
