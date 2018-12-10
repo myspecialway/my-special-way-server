4920311231f7dd270fd25329ef484e00dfc48407
Liberman, Pablo (pl1890) <pl1890@att.com>
2018-12-10 23:13:52 +0200

    added temporal field to scheduler

--- src/models/temporal.db.mode.ts
+++ src/models/temporal.db.mode.ts
@@ -1,13 +1,9 @@
 import { LessonDbModel } from './lesson.db.model';
 import LocationDbModel from './location.db.model';
-import { TemporalDbData } from './temporal.db.mode';
 
 // Describes a single timeslot on the schedule
-export interface TimeSlotDbModel {
-  index: string;
-  hours: string;
-  temporal?: TemporalDbData;
+export interface TemporalDbData {
+  expired: Date;
   lesson?: LessonDbModel;
   room?: LocationDbModel;
-  customized?: boolean;
 }
