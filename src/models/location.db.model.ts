8424ad392b7a78491e292f748ffe874ba1bace63
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-12-25 15:56:24 +0200

    model update

--- src/models/location.db.model.ts
+++ src/models/location.db.model.ts
@@ -2,7 +2,6 @@ import Point from './point.db.model';
 export default interface LocationDbModel {
   _id: string;
   name: string;
-  disabled: boolean;
   location_id: string;
   position: Point;
   icon: string;
