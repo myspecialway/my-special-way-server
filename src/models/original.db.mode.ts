c28cdd988a348079f5f56f32d498ba5b3789ec67
YossiAtt <yg771u@intl.att.com>
2018-12-30 14:55:28 +0200

    [maint] - change name from temporal to origin

--- src/models/original.db.mode.ts
+++ src/models/original.db.mode.ts
@@ -2,7 +2,7 @@ import { LessonDbModel } from './lesson.db.model';
 import LocationDbModel from './location.db.model';
 
 // Describes a single timeslot on the schedule
-export interface TemporalDbData {
+export interface OriginalDbData {
   expired: Date;
   lesson?: LessonDbModel;
   room?: LocationDbModel;
--- src/models/timeslot.db.model.ts
+++ src/models/timeslot.db.model.ts
@@ -1,12 +1,12 @@
 import { LessonDbModel } from './lesson.db.model';
 import LocationDbModel from './location.db.model';
-import { TemporalDbData } from './temporal.db.mode';
+import { OriginalDbData } from './original.db.mode';
 
 // Describes a single timeslot on the schedule
 export interface TimeSlotDbModel {
   index: string;
   hours: string;
-  temporal?: TemporalDbData;
+  original?: OriginalDbData;
   lesson?: LessonDbModel;
   room?: LocationDbModel;
   customized?: boolean;
