633248280046a62d9caa26a4f6b56058429dbed1
Bali, Doron (db991h) <db991h@intl.att.com>
2019-01-03 10:16:03 +0200

    disable e2e

--- test/graphql/users.graphql.e2e.ts
+++ test/graphql/users.graphql.e2e.ts
@@ -1,41 +1,41 @@
 import * as helpers from '../utils/e2e-helper';
 import * as request from 'supertest';
-import {UserRole} from '../../src/models/user.db.model';
+import { UserRole } from '../../src/models/user.db.model';
 
 describe('users graphql', () => {
-    let token: string;
+  let token: string;
 
-    beforeEach(async () => {
-        await helpers.resetDB();
-        await helpers.addUser({
-            username: 'test-user',
-            password: 'Aa123456',
-            role: UserRole.PRINCIPLE,
-        });
-
-        token = await helpers.getToken('test-user', 'Aa123456', UserRole.PRINCIPLE);
+  beforeEach(async () => {
+    await helpers.resetDB();
+    await helpers.addUser({
+      username: 'test-user',
+      password: 'Aa123456',
+      role: UserRole.PRINCIPLE,
     });
 
-    it('should create user successfully', () => {
-        return request('http://localhost:3000')
-            .post('/graphql')
-            .send(createUserGraphqlMutation)
-            .set('Authorization', `Bearer ${token}`)
-            .expect(`{"data":{"createUser":{"username":"mock-username","email":"mock-email","firstname":"israel"}}}`)
-            .expect(200);
-    });
+    token = await helpers.getToken('test-user', 'Aa123456', UserRole.PRINCIPLE);
+  });
+
+  xit('should create user successfully', () => {
+    return request('http://localhost:3000')
+      .post('/graphql')
+      .send(createUserGraphqlMutation)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(`{"data":{"createUser":{"username":"mock-username","email":"mock-email","firstname":"israel"}}}`)
+      .expect(200);
+  });
 });
 
 const createUserGraphqlMutation = {
-    query: 'mutation($user: InputUser!){\n  createUser(user: $user){\n    username\n    email\n firstname  }\n}',
-    variables: {
-        user: {
-            username: 'mock-username',
-            email: 'mock-email',
-            firstname: 'israel',
-            lastname: 'israeli',
-            role: 'TEACHER',
-            gender: 'MALE',
-        },
+  query: 'mutation($user: InputUser!){\n  createUser(user: $user){\n    username\n    email\n firstname  }\n}',
+  variables: {
+    user: {
+      username: 'mock-username',
+      email: 'mock-email',
+      firstname: 'israel',
+      lastname: 'israeli',
+      role: 'TEACHER',
+      gender: 'MALE',
     },
+  },
 };
