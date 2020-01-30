3ba100acccf5057cb09187c6c2e163f17c78be3e
KfirMor <kfir.mor@gmail.com>
2020-01-30 17:23:27 +0200

    Added the UN/PW to the config interface

--- src/config/config-interface.ts
+++ src/config/config-interface.ts
@@ -7,6 +7,8 @@ export interface ProcessEnvConfig {
   PAPERTRAIL_HOST_PORT: string;
   BASE_URL: string;
   EXPIRATION_FIRST_TOKEN_MINUTES: string;
+  EMAIL_CLIENT_USERNAME: string;
+  EMAIL_CLIENT_PASSWORD: string;
 }
 
 export function getEnvalidValidations(): { [K in keyof ProcessEnvConfig]: envalid.ValidatorSpec<ProcessEnvConfig[K]> } {
@@ -17,5 +19,7 @@ export function getEnvalidValidations(): { [K in keyof ProcessEnvConfig]: envali
     PAPERTRAIL_HOST_PORT: envalid.str({ default: 'logs7.papertrailapp.com:32979' }),
     BASE_URL: envalid.str({ default: 'http://mswppr.tel-aviv.gov.il' }),
     EXPIRATION_FIRST_TOKEN_MINUTES: envalid.str({ default: '15' }),
+    EMAIL_CLIENT_USERNAME: envalid.str(),
+    EMAIL_CLIENT_PASSWORD: envalid.str(),
   };
 }
